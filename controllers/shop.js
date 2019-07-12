ecf02c6d77937eaee24248fcaee1a2c700807e16
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2019-07-12 15:42:17 -0400

    Build cart model

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/products');
+const Cart = require('../models/cart');
 
 exports.getIndex = (req, res, next) => {
   Product.fetchAll((products) => {
@@ -40,6 +41,9 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
   const productId = req.params.productId;
+  Product.findById(productId, product => {
+    Cart.addProduct(productId, product.price);
+  });
   res.redirect('/cart');
 };
 
